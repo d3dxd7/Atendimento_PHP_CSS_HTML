17f6e0796fe9e5900c47f46f87a38de55719c5d7
unknown <lucas.matheusgorni@hotmail.com>
2023-05-30 20:49:36 -0300

    Atualizacao e criacao da pagina de Relatorio

--- formulario/gerenciamento_atendimento.php
+++ formulario/gerenciamento_atendimento.php
@@ -1,53 +1,97 @@
-<?php
-// Configurações do banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gerenciamento_atendimento";
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="main.css">
+    <title>Resultados</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
 
+        table, th, td {
+            border: 1px solid black;
+            padding: 5px;
+            border-color:aqua;
+        }
+    </style>
+</head>
+<body class="body-result">
+    <div class="div-result">
+        <fieldset>
+        <form method="GET" action="">
+        <legend class="legend-gerencia"><b>Relatorio de Atendimento</b></legend>
+                <br>
+                <br>
+            <div class="div-gerencia">
+                <label for="data">Selecione data Atendimento (Dia-Mes-Ano):</label>
+                <input type="date" id="data_nasc" name="data">
+                <button type="submit" id="submit-gerencia">Pesquisar</button>
+                <button type="button" class="btn-voltar">
+                    <a href="index.php" class="btn-voltar-link"><b>Voltar</b></a>
+                </button>
+            </div>
+        </form>
+        <br>
 
-// Conexão com o banco de dados
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+        <?php
+        // Configurações do banco de dados
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "gerenciamento_atendimento";
 
-// Verifica se a conexão foi estabelecida corretamente
-if (!$conn) {
-    die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
-}
+        // Conexão com o banco de dados
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Consulta SQL
-$sql = "SELECT id, nome_cliente, cidade_municipio, sistema_usado, telefone, sexo, data_nascimento FROM gerenciamento_atendimento";
-$result = mysqli_query($conn, $sql);
+        // Verifica se a conexão foi estabelecida corretamente
+        if (!$conn) {
+            die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+        }
 
-// Verifica se a consulta retornou resultados
-if (mysqli_num_rows($result) > 0) {
-    // Exibe os resultados em uma tabela HTML
-    echo "<table>";
-    echo "<tr>
-            <th>ID</th>
-            <th>Nome do Cliente</th>
-            <th>Cidade/Município</th>
-            <th>Sistema Usado</th>
-            <th>Telefone</th>
-            <th>Sexo</th>
-            <th>Data de Nascimento</th>
-          </tr>";
-          
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td>" . $row["nome_cliente"] . "</td>";
-        echo "<td>" . $row["cidade_municipio"] . "</td>";
-        echo "<td>" . $row["sistema_usado"] . "</td>";
-        echo "<td>" . $row["telefone"] . "</td>";
-        echo "<td>" . $row["sexo"] . "</td>";
-        echo "<td>" . $row["data_nascimento"] . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "Nenhum registro encontrado.";
-}
+        // Verifica se foi enviada uma data pelo formulário
+        if (isset($_GET['data'])) {
+            $data = $_GET['data'];
 
-// Fecha a conexão com o banco de dados
-mysqli_close($conn);
-?>
+            // Consulta SQL com filtro de data
+            $sql = "SELECT id, nome_cliente, cidade_municipio, sistema_usado, telefone, sexo, data_nascimento FROM formulario WHERE data_nascimento = '$data'";
+            $result = mysqli_query($conn, $sql);
+
+            // Verifica se a consulta retornou resultados
+            if (mysqli_num_rows($result) > 0) {
+                // Exibe os resultados em uma tabela HTML
+                echo "<table>";
+                echo "<tr>
+                        <th>ID</th>
+                        <th>Nome do Cliente</th>
+                        <th>Cidade/Município</th>
+                        <th>Sistema Usado</th>
+                        <th>Telefone</th>
+                        <th>Sexo</th>
+                        <th>Data de Nascimento</th>
+                      </tr>";
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row["id"] . "</td>";
+                    echo "<td>" . $row["nome_cliente"] . "</td>";
+                    echo "<td>" . $row["cidade_municipio"] . "</td>";
+                    echo "<td>" . $row["sistema_usado"] . "</td>";
+                    echo "<td>" . $row["telefone"] . "</td>";
+                    echo "<td>" . $row["sexo"] . "</td>";
+                    echo "<td>" . $row["data_nascimento"] . "</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "Nenhum registro encontrado para a data especificada.";
+            }
+        }
+
+        // Fecha a conexão com o banco de dados
+        mysqli_close($conn);
+        ?>
+        </fieldset>
+        <label for="" class="credit-gerencia">®Copyright Lucas Matheus - Amendola e Amendola LTDA</label>
+    </div>
+</body>
+</html>
--- formulario/index.php
+++ formulario/index.php
@@ -2,18 +2,18 @@
 //Se Existir essa Variavel [submit] = faca
     if(isset($_POST['submit']))
     {
-        //print_r('nome '.$_POST['nome']);
-        //print_r('<br>');
-        //print_r('cidade '.$_POST['cidade']);
-        //print_r('<br>');
-        //print_r('sistema-usado '.$_POST['sistema-usado']);
-        //print_r('<br>');
-        //print_r('tel-cel '.$_POST['tel-cel']);
-        //print_r('<br>');
-        //print_r('sexo '.$_POST['genero']);
-        //print_r('<br>');
-        //print_r('tel-cel'.$_POST['data_nasc']);
-        //print_r('<br>');
+        print_r('nome '.$_POST['nome']);
+        print_r('<br>');
+        print_r('cidade '.$_POST['cidade']);
+        print_r('<br>');
+        print_r('sistema-usado '.$_POST['sistema-usado']);
+        print_r('<br>');
+        print_r('tel-cel '.$_POST['tel-cel']);
+        print_r('<br>');
+        print_r('sexo '.$_POST['genero']);
+        print_r('<br>');
+        print_r('data_nasc '.$_POST['data_nasc']);
+        print_r('<br>');
         include_once('config.php');
 
         $nome = $_POST['nome'];
@@ -23,10 +23,24 @@
         $genero = $_POST['genero'];
         $data_nasc = $_POST['data_nasc'];
 
+        //INSERT INTO formulario(nome_cliente, cidade_municipio, sistema_usado, telefone,
+        //sexo, data_nascimento)VALUES ('lucas matheus','sales-sp','sis-sistema saude','17 - 9999-9999','Masculino','1998-04-17')
 
-        $resultado = mysqli_query($conexao, "INSERT INTO formulario(nome_cliete, cidade_municipio, sistema_usado, telefone,
-        sexo, date_nascimento)VALUES ($nome, $cidade, $sistema_usado, $tel_cel, $genero, $data_nasc)");
-
+        //$resultado = mysqli_query($conexao, "INSERT INTO formulario(nome_cliente, cidade_municipio, sistema_usado, telefone,
+        //sexo, data_nascimento)VALUES ($nome, $cidade, $sistema_usado, $tel_cel, $genero, $data_nasc)");
+        
+        // Preparar a consulta com prepared statements
+        $consulta = $conexao->prepare("INSERT INTO formulario(nome_cliente, cidade_municipio, sistema_usado, telefone, sexo, data_nascimento) VALUES (?, ?, ?, ?, ?, ?)");
+        // Vincular os parâmetros da consulta
+        $consulta->bind_param("ssssss", $nome, $cidade, $sistema_usado, $tel_cel, $genero, $data_nasc);
+        // Executar a consulta
+        $resultado = $consulta->execute();
+    
+        if ($resultado) {
+            echo "Dados inseridos com sucesso!";
+        } else {
+            echo "Erro ao inserir os dados: " . $conexao->error;
+        }
     }
 ?>
 
@@ -47,7 +61,7 @@
                 <legend><b>Formulario de Atendimento</b></legend>
                 <br>
                 <label for="" class="sistema">SIS - SAS - SIE - SSE</label>
-                <br>
+                <br><br>
                 <br>
                 <div class="inputBox">
                     <input type="text" name="nome" id="nome" class="input-user" required>
@@ -72,10 +86,10 @@
                 <p class="">Sexo:
                 <br>
                 <br>
-                <input type="radio" name="genero" id="genero" value="feminino" required>
+                <input type="radio" name="genero" id="genero" value="Feminino" required>
                 <label for="feminino">Feminino</label>
                 <br>
-                <input type="radio" name="genero" id="genero" value="feminino" required>
+                <input type="radio" name="genero" id="genero" value="Masculino" required>
                 <label for="masculino">Masculino</label>
                 <br>
                 <input type="radio" name="genero" id="genero" value="outro" required>
@@ -83,12 +97,14 @@
                 </p>
                 <hr>
                 <br>
-                <label for="data_nasc"><b>Data do Atendimento</b></label>
+                <label for="data_nasc" id="data"><b>Data do Atendimento</b></label>
                 <input type="date" name="data_nasc" id="data_nasc" required>
                 <br><br>
                 <input type="submit" name="submit" id="submit">
                 <br><br>
-                <button id="submit" href="">Consultar Clientes</button>
+                <button id="submit" href="">
+                    <a href="gerenciamento_atendimento.php" id="submit-gerenciamento">Consultar Clientes</a>
+                </button>
             </fieldset>
             <label for="" class="credit">®Copyright Lucas Matheus - Amendola e Amendola LTDA</label>
         </form>
